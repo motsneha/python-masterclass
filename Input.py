a1f72775471991c5350869adb0a98aac06d9a5f3
motsneha <motsneha@gmail.com>
2018-09-01 18:43:03 +0530

    variables, input

---
+++ Input.py
@@ -0,0 +1,11 @@
+# greeting = "Hello"
+# name = input("Please enter your name ")
+# print(greeting + " " + name)
+
+spiltString = "This string has been\nsplit over\nseveral\nlines"
+print(spiltString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print (""" THis is also working """)
---
+++ Variables.py
@@ -0,0 +1,22 @@
+greeting = "Hello"
+_name = "Sneha"
+Greeting = "Morning"
+This_1_is_valid = "Yay"
+var5 = "Fifth Variable"
+
+print(greeting + " " + _name)
+
+age = 5
+
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+for i in range (1, a//b):
+    print(i)
